56572e698b2d2fade0d3a88b06fc91dd5d4a0866
Renzo Jimenez Alfaro <hijo72@hotmail.com>
2024-11-20 20:37:36 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM  from 'react-dom/client';
 import './assets/index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root') as HTMLElement;
-if (rootElement) {
+const rootElement = document.getElementById('root')!;
+if (rootElement instanceof HTMLElement) {
   const root : ReactDOM.Root = ReactDOM.createRoot(rootElement)  as ReactDOM.Root;
   root.render(
     <App />
